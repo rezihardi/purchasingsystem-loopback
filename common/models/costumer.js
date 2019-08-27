d7bc75043b92437967e1d477014f41f6f2e5aacb
rezihardi <rezydoerasta@gmail.com>
2019-08-27 13:40:52 +0700

    firstcommit

---
+++ common/models/costumer.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function (Costumer) {
+
+  //remote method
+  Costumer.remoteMethod(
+    'getNameLike', {
+      description: 'get name like',
+      accepts: [{
+        arg: 'firstname',
+        type: 'string'
+      }],
+      returns: {
+        arg: 'res',
+        type: 'object',
+        root: true
+      },
+      http: {
+        path: '/getNameLike',
+        verb: 'get'
+      }
+    }
+  );
+
+
+  //fungsi getNameLike - > get name with first name param
+  Costumer.getNameLike = function (firstname, callback) {
+    new Promise(function (resolve, reject) {
+
+      //query filter variable
+      let filter = {
+        where: {
+          firstname: {
+            like: firstname
+          }
+        }
+      }
+
+      // querying data
+      Costumer.find(filter, function (err, result) {
+        if (err) reject(err)
+        if (result === null) {
+          err = new Error("Nama Tidak Dapat Ditemukan")
+          err.statusCode = 404
+          reject(err)
+        }
+        resolve(result)
+      })
+    }).then(function (res) {
+      //callback result
+      if (!res) callback(err)
+      return callback(null, res)
+    }).catch(function (err) {
+      callback(err);
+    });
+  }
+};
